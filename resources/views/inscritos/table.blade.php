00f4710c33e7cad6f627b586712cccb6ebc621a1
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2018-07-15 15:21:57 -0300

    Removendo views duplicadas]

--- resources/views/inscritos/table.blade.php
+++ resources/views/inscritos/table.blade.php
@@ -1,10 +1,10 @@
 @section('css')
-    @include('layouts.datatables_css')
+    @include('admin.layouts.datatables_css')
 @endsection
 
 {!! $dataTable->table(['width' => '100%']) !!}
 
 @section('scripts')
-    @include('layouts.datatables_js')
+    @include('admin.layouts.datatables_js')
     {!! $dataTable->scripts() !!}
 @endsection
--- resources/views/layouts/head.blade.php
+++
@@ -1,9 +0,0 @@
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-
-	<title>IX Congresso Paulista da ABENEPI</title>
-
-	<link rel="stylesheet" href="css/app.css">
-</head>
