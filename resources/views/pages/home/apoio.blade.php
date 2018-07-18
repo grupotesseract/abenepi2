3a0cda29aed128240cc4453c9c1151a179d2fc33
Leandro Acquati <leandroacquati@gmail.com>
2018-07-18 00:23:17 -0300

    ajustes finos no site e colocando imagens no cludinary

--- resources/views/pages/home/apoio.blade.php
+++ resources/views/pages/home/apoio.blade.php
@@ -1,34 +1,44 @@
 <div class="container apoio" id="apoio">
 	<h1 class="text-center mb-5">Apoio</h1>
 
-	<div class="row">
+	<div class="row justify-content-center mb-5">
 		<div class="col-12 col-lg-4">
 			<a href="https://disapre.wordpress.com/" target="_blank">
-				<img src="images/disapre.png">
-			</a>
-		</div>
-
-		<div class="col-12 col-lg-4">
-			<a href="http://ciapre.org.br/" target="_blank">
-				<img src="images/ciapre.png">
+				<div class="logo">
+					<img src="https://res.cloudinary.com/tesseract/image/upload/f_auto,q_auto/v1531876306/abenepi2/disapre.png">
+				</div>
 			</a>
 		</div>
 
 		<div class="col-12 col-lg-4">
 			<a href="https://www.shire.com.br/" target="_blank">
-				<img src="images/shire.png">
+				<div class="logo">
+					<img src="https://res.cloudinary.com/tesseract/image/upload/f_auto,q_auto/v1531876306/abenepi2/shire.png">
+				</div>
 			</a>
 		</div>
 
-		<div class="col-12 col-lg-6">
-			<a href="https://www.booktoy.com.br/" target="_blank">
-				<img src="images/booktoy.png">
-			</a>
-		</div>
-
-		<div class="col-12 col-lg-6">
+		<div class="col-12 col-lg-4">
 			<a href="https://www.grupotesseract.com.br/" target="_blank">
-				<img class="tesseract" src="images/tesseract.png">
+				<div class="logo">
+					<img class="tesseract" src="https://res.cloudinary.com/tesseract/image/upload/f_auto,q_auto/v1531876306/abenepi2/logo-tesseract-preto.png">
+				</div>
+			</a>
+		</div>
+
+		<div class="col-12 col-lg-4">
+			<a href="https://www.booktoy.com.br/" target="_blank">
+				<div class="logo">
+					<img src="https://res.cloudinary.com/tesseract/image/upload/f_auto,q_auto/v1531876306/abenepi2/booktoy.png">
+				</div>
+			</a>
+		</div>
+
+		<div class="col-12 col-lg-4">
+			<a href="http://ciapre.org.br/" target="_blank">
+				<div class="logo">
+					<img src="https://res.cloudinary.com/tesseract/image/upload/f_auto,q_auto/v1531876306/abenepi2/ciapre.png">
+				</div>			
 			</a>
 		</div>
 	</div>
